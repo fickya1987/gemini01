942fd4beef8318ce91431aa1568646673e85597f
fickya1987 <89722540+fickya1987@users.noreply.github.com>
2024-08-05 22:20:25 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import streamlit as st
+import os
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+
+
+
+def get_gemini_response(question):
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(question)
+    return response.text
+
+# initialise our streamlit app
+
+st.set_page_config(page_title="Q&A Demo")
+
+st.header("Gemini LLM Application")
+
+input=st.text_input("Input: ", key="input")
+submit = st.button("Ask the question")
+
+
+
+if submit:
+    response=get_gemini_response(input)
+    st.subheader("The Response is ")
+    st.write(response)
